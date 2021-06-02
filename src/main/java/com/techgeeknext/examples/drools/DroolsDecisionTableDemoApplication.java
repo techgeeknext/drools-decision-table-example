2c4cde9287f660e843cf3415772692fe98ea67e0
TechGeekNeXt <techgeeknext@gmail.com>
2021-06-02 16:11:35 +0530

    https://www.techgeeknext.com/spring-boot/drools-decision-table-example

---
+++ src/main/java/com/techgeeknext/examples/drools/DroolsDecisionTableDemoApplication.java
@@ -0,0 +1,13 @@
+package com.techgeeknext.examples.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsDecisionTableDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsDecisionTableDemoApplication.class, args);
+    }
+
+}
